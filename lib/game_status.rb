1aa0750485ba8957a07a3e26da838c0c9a68573a
Khaled Ali Ahmed <s.echo.bird@gmail.com>
2019-05-04 16:32:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,94 +4,58 @@ def position_taken?(board, index)
 end
 
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
+  [0,1,2],
+  [3,4,5],
   [6,7,8],
   [0,3,6],
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [2,4,6]
+  [6,4,2]
 ]
 
-def empty?(board)
-  eboard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  if board == eboard
-    return true
-  end
-end
-
-def won?(board)
-  if empty?(board)
-    return false
-  end
-
-  for win_combination in WIN_COMBINATIONS
-    # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-    # grab each index from the win_combination that composes a win.
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-    xwin = (position_1 == "X" && position_2 == "X" && position_3 == "X")
-    owin = (position_1 == "O" && position_2 == "O" && position_3 == "O")
-    win = (xwin || owin)
-    if win
-      return win_combination # return the win_combination indexes that won.
-    elsif full?(board)
+def won?(rows)
+  if (rows.count{ |i| i == ' '} === 9)
+    false
+  else
+    if (rows[0] == 'X' && rows[1] == 'X' && rows[2] == 'X') || (rows[0] == 'O' && rows[1] == 'O' && rows[2] == 'O')
+      return WIN_COMBINATIONS[0]
+    elsif (rows[3] == 'X' && rows[4] == 'X' && rows[5] == 'X') || (rows[3] == 'O' && rows[4] == 'O' && rows[5] == 'O')
+      return WIN_COMBINATIONS[1]
+    elsif (rows[6] == 'X' && rows[7] == 'X' && rows[8] == 'X') || (rows[6] == 'O' && rows[7] == 'O' && rows[8] == 'O')
+      return WIN_COMBINATIONS[2]
+    elsif (rows[0] == 'X' && rows[3] == 'X' && rows[6] == 'X') || (rows[0] == 'O' && rows[3] == 'O' && rows[6] == 'O')
+      return WIN_COMBINATIONS[3]
+    elsif (rows[1] == 'X' && rows[4] == 'X' && rows[7] == 'X') || (rows[1] == 'O' && rows[4] == 'O' && rows[7] == 'O')
+      return WIN_COMBINATIONS[4]
+    elsif (rows[2] == 'X' && rows[5] == 'X' && rows[8] == 'X') || (rows[2] == 'O' && rows[5] == 'O' && rows[8] == 'O')
+      return WIN_COMBINATIONS[5]
+    elsif (rows[0] == 'X' && rows[4] == 'X' && rows[8] == 'X') || (rows[0] == 'O' && rows[4] == 'O' && rows[8] == 'O')
+      return WIN_COMBINATIONS[6]
+    elsif (rows[6] == 'X' && rows[4] == 'X' && rows[2] == 'X') || (rows[6] == 'O' && rows[4] == 'O' && rows[2] == 'O')
+      return WIN_COMBINATIONS[7]
+    else
       return false
     end
   end
 end
 
 def full?(board)
-  9.times do |index|
-    if !position_taken?(board, index)
-      return false
-    end
-  end
-  return true
+  board.count{|i| i != ' '} == 9
 end
 
 def draw?(board)
-  if won?(board)
-    return false
-  end
-
-  if full?(board)
-    return true
-  end
-
-  if !won?(board) && !full?(board)
-    return false
-  end
+  return true if !won?(board) && full?(board)
+  return false unless (won?(board) && full?(board)) || won?(board)
 end
-
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board)
-    return true
-  else
-    return false
-  end
+return true if won?(board) || draw?(board) || full?(board)
+false
 end
 
-
 def winner(board)
-  if full?(board) || !won?(board)
-    return nil
-  end
-  win_combination = won?(board)
-  b = board[win_combination[0]]
-  if b == "X"
-    return "X"
-  elsif b == "O"
-    return "O"
-  else
-    return nil
-  end
+  return nil if board.count('X') == board.count('O')
+  board.count('X') > board.count('O') ? 'X' : 'O'
+
 end
